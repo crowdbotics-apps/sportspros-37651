9e2173689b99046108914868f398641ebb955512
josephicey <josephicey@gmail.com>
2022-11-22 02:47:55 +0530

    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,9 +1,13 @@
+import { Pressable } from "react-native";
+import { TextInput } from "react-native";
 import React from "react";
 import { View, StyleSheet } from "react-native";
 
-const Untitled1 = () => {
+const Untitled1 = ({
+  navigation
+}) => {
   return <View style={_styles.IeJqThhU}>
-      </View>;
+      <Pressable onPress={() => navigation.navigate("Untitled2")} style={_styles.uFckDsuG}><View style={_styles.IDBpaDue}></View></Pressable><TextInput style={_styles.EUBfkpRA}></TextInput></View>;
 };
 
 export default Untitled1;
@@ -15,5 +19,27 @@ const _styles = StyleSheet.create({
     position: "relative",
     height: "100%",
     borderRadius: 0
+  },
+  IDBpaDue: {
+    left: 81,
+    top: 303,
+    position: "absolute",
+    height: 50,
+    width: 228,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  },
+  EUBfkpRA: {
+    left: 58,
+    top: 180,
+    position: "absolute",
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 241,
+    height: 30
+  },
+  uFckDsuG: {
+    position: "unset"
   }
 });
---
+++ screens/Untitled2/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled2 = () => {
+  return <View style={_styles.lTMTbdhs}>
+      </View>;
+};
+
+export default Untitled2;
+
+const _styles = StyleSheet.create({
+  lTMTbdhs: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
